4a7a4ab0117695f9be505906e4af1f48f67392bf
Fazril <fazrilazam@gmail.com>
2025-06-15 23:16:09 +0800

    Added Devices Table

---
+++ app/Models/Device.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Device extends Model
+{
+    use HasFactory;
+
+    // Fillable fields
+    protected $fillable = [
+        'name',
+        'type',
+        'description',
+        'images',
+        'status',
+    ];
+}
